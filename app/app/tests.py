41a7c692e172114fd853602ac983c3021f25f41b
Jonathon Francis <jogofo@gmail.com>
2022-11-04 00:03:57 +1100

    db config

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """test calc"""
+
+    def test_add_nubers(self):
+        """test add numbers"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """ test subtract"""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
