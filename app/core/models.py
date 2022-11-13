739745402698b6eba0d1e5522a45d905029ed16e
Jonathon Francis <jogofo@gmail.com>
2022-11-13 14:30:49 +1100

    Create Superuser

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@
 Database models.
 """
 
+
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -15,8 +16,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, safe and retrurn a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("User must have an email address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
